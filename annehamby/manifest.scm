169a40b70a50e17a4e26d27d568972243575f382
Kyle Shannon <kyle@pobox.com>
2024-06-28 09:25:56 -0600

    annehamby: add container

---
+++ annehamby/manifest.scm
@@ -0,0 +1,21 @@
+(specifications->manifest (list
+                            ;; base packages
+                            "bash-minimal"
+                            "glibc-locales"
+                            "nss-certs"
+                            ;; Common command line tools lest the container is too empty.
+                            "coreutils"
+                            "grep"
+                            "which"
+                            "wget"
+                            "sed"
+                            ;; Toolchain and common libraries for "install.packages"
+                            "gcc-toolchain@10"
+                            "gfortran-toolchain"
+                            "cmake"
+                            "git"
+                            "make"
+                            ;; R packages
+                            "r-minimal"
+                            "r-tidyverse"
+                          ))
