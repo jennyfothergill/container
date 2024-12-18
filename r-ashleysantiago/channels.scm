bd6cdc181c03e0d2941bb614a06573e3ab3300a4
Jenny Fothergill <jennyfothergill@boisestate.edu>
2024-12-18 16:58:20 -0700

    Try updating channels

--- r-ashleysantiago/channels.scm
+++ r-ashleysantiago/channels.scm
@@ -1,28 +1,62 @@
 (list (channel
-        (name 'guix-cran)
-        (url "https://github.com/guix-science/guix-cran.git")
+        (name 'guix)
+        (url "https://git.savannah.gnu.org/git/guix.git")
         (branch "master")
         (commit
-          "466d876bcc15a1a6c2053eb150548f7f471f75b7"))
+          "1a62f687e504c67f26693f3bc60a90e503ab4ea9")
+        (introduction
+          (make-channel-introduction
+            "9edb3f66fd807b096b48283debdcddccfea34bad"
+            (openpgp-fingerprint
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+      (channel
+        (name 'guix-science-nonfree)
+        (url "https://codeberg.org/guix-science/guix-science-nonfree.git")
+        (branch "master")
+        (commit
+          "6e78c0fe191a227535c5afa56b344aba15aa4411")
+        (introduction
+          (make-channel-introduction
+            "58661b110325fd5d9b40e6f0177cc486a615817e"
+            (openpgp-fingerprint
+              "CA4F 8CF4 37D7 478F DA05  5FD4 4213 7701 1A37 8446"))))
+      (channel
+        (name 'guix-past)
+        (url "https://codeberg.org/guix-science/guix-past.git")
+        (branch "master")
+        (commit
+          "2d3485b7fd7c1904bc7c1a87fc45048376ff4d3a")
+        (introduction
+          (make-channel-introduction
+            "0c119db2ea86a389769f4d2b9c6f5c41c027e336"
+            (openpgp-fingerprint
+              "3CE4 6455 8A84 FDC6 9DB4  0CFB 090B 1199 3D9A EBB5"))))
       (channel
         (name 'guix-science)
-        (url "https://github.com/guix-science/guix-science.git")
+        (url "https://codeberg.org/guix-science/guix-science.git")
         (branch "master")
         (commit
-          "7c6ca7e1c19c92bfe5e6add688662e08b07db2bf")
+          "33add622caaa24efe8b0fa7274939d139e4f0d34")
         (introduction
           (make-channel-introduction
             "b1fe5aaff3ab48e798a4cce02f0212bc91f423dc"
             (openpgp-fingerprint
               "CA4F 8CF4 37D7 478F DA05  5FD4 4213 7701 1A37 8446"))))
       (channel
-        (name 'guix)
-        (url "https://git.savannah.gnu.org/git/guix.git")
+        (name 'guix-bioc)
+        (url "https://github.com/guix-science/guix-bioc.git")
         (branch "master")
         (commit
-          "d616e85ac8a380d5126da3184f7037c20866c884")
-        (introduction
-          (make-channel-introduction
-            "9edb3f66fd807b096b48283debdcddccfea34bad"
-            (openpgp-fingerprint
-              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
+          "93aace58ce4915f911a24f68e2aa31ef6b2725ad"))
+      (channel
+        (name 'guix-hpc)
+        (url "https://gitlab.inria.fr/guix-hpc/guix-hpc.git")
+        (branch "master")
+        (commit
+          "4fe13181c1498c1ced7be97912ed92dca49802a1"))
+      (channel
+        (name 'guix-cran)
+        (url "https://github.com/guix-science/guix-cran.git")
+        (branch "master")
+        (commit
+          "47f2580b12d3c7783c774105208eeb7eec65362a")))
