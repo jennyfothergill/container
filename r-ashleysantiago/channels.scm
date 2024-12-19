ddc302bb1f7cedd61b1106ef7e84584ac1764f79
Jenny Fothergill <jennyfothergill@boisestate.edu>
2024-12-18 17:21:16 -0700

    Use channels from peregrine container minus BSU variant

--- r-ashleysantiago/channels.scm
+++ r-ashleysantiago/channels.scm
@@ -1,62 +1,62 @@
 (list (channel
-        (name 'guix)
-        (url "https://git.savannah.gnu.org/git/guix.git")
+        (name 'guix-cran)
+        (url "https://github.com/guix-science/guix-cran.git")
         (branch "master")
         (commit
-          "1a62f687e504c67f26693f3bc60a90e503ab4ea9")
-        (introduction
-          (make-channel-introduction
-            "9edb3f66fd807b096b48283debdcddccfea34bad"
-            (openpgp-fingerprint
-              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+          "963e4b0d66834f0f1bf3fc4b6b7b4bf323266359"))
       (channel
-        (name 'guix-science-nonfree)
-        (url "https://codeberg.org/guix-science/guix-science-nonfree.git")
+        (name 'guix-hpc)
+        (url "https://gitlab.inria.fr/guix-hpc/guix-hpc.git")
         (branch "master")
         (commit
-          "6e78c0fe191a227535c5afa56b344aba15aa4411")
-        (introduction
-          (make-channel-introduction
-            "58661b110325fd5d9b40e6f0177cc486a615817e"
-            (openpgp-fingerprint
-              "CA4F 8CF4 37D7 478F DA05  5FD4 4213 7701 1A37 8446"))))
+          "971f60b2854afb8b7dff77096c0f0f3149ee5070"))
       (channel
-        (name 'guix-past)
-        (url "https://codeberg.org/guix-science/guix-past.git")
+        (name 'guix-bioc)
+        (url "https://github.com/guix-science/guix-bioc.git")
         (branch "master")
         (commit
-          "2d3485b7fd7c1904bc7c1a87fc45048376ff4d3a")
-        (introduction
-          (make-channel-introduction
-            "0c119db2ea86a389769f4d2b9c6f5c41c027e336"
-            (openpgp-fingerprint
-              "3CE4 6455 8A84 FDC6 9DB4  0CFB 090B 1199 3D9A EBB5"))))
+          "5a63fc722773efa87cb56e89dc88804726b95728"))
       (channel
         (name 'guix-science)
         (url "https://codeberg.org/guix-science/guix-science.git")
         (branch "master")
         (commit
-          "33add622caaa24efe8b0fa7274939d139e4f0d34")
+          "7304e37d4d664f27a18577e388c91413878d62fc")
         (introduction
           (make-channel-introduction
             "b1fe5aaff3ab48e798a4cce02f0212bc91f423dc"
             (openpgp-fingerprint
               "CA4F 8CF4 37D7 478F DA05  5FD4 4213 7701 1A37 8446"))))
       (channel
-        (name 'guix-bioc)
-        (url "https://github.com/guix-science/guix-bioc.git")
+        (name 'guix-science-nonfree)
+        (url "https://codeberg.org/guix-science/guix-science-nonfree.git")
         (branch "master")
         (commit
-          "93aace58ce4915f911a24f68e2aa31ef6b2725ad"))
+          "0505db27f162053bc917f527ec4026bec9031dff")
+        (introduction
+          (make-channel-introduction
+            "58661b110325fd5d9b40e6f0177cc486a615817e"
+            (openpgp-fingerprint
+              "CA4F 8CF4 37D7 478F DA05  5FD4 4213 7701 1A37 8446"))))
       (channel
-        (name 'guix-hpc)
-        (url "https://gitlab.inria.fr/guix-hpc/guix-hpc.git")
+        (name 'guix)
+        (url "https://git.savannah.gnu.org/git/guix.git")
         (branch "master")
         (commit
-          "4fe13181c1498c1ced7be97912ed92dca49802a1"))
+          "b10ce47d8be5b6c15987f4e1d93b3bd71b1eb220")
+        (introduction
+          (make-channel-introduction
+            "9edb3f66fd807b096b48283debdcddccfea34bad"
+            (openpgp-fingerprint
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
       (channel
-        (name 'guix-cran)
-        (url "https://github.com/guix-science/guix-cran.git")
+        (name 'guix-past)
+        (url "https://codeberg.org/guix-science/guix-past.git")
         (branch "master")
         (commit
-          "47f2580b12d3c7783c774105208eeb7eec65362a")))
+          "f99ada4123de1eadf668d34dac2d726407634549")
+        (introduction
+          (make-channel-introduction
+            "0c119db2ea86a389769f4d2b9c6f5c41c027e336"
+            (openpgp-fingerprint
+              "3CE4 6455 8A84 FDC6 9DB4  0CFB 090B 1199 3D9A EBB5")))))
