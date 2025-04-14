f58c52d8b7f1dad34fe1d75775b025e6c77112b2
Jenny Fothergill <jennyfothergill@boisestate.edu>
2025-04-14 14:54:48 -0600

    Add runjags dependency

--- r-louisjochems/manifest.scm
+++ r-louisjochems/manifest.scm
@@ -55,4 +55,5 @@
                             "r-rworldmap"
                             "r-ecfun"
                             "r-ecdat"
+                            "r-jags"
                           ))
